2a1a1137d37f102e55152eeadc49469619609ad3
mirsadkt <mirsadkt@gmail.com>
2023-12-03 10:23:07 +0530

    third commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,43 @@
+// ignore_for_file: avoid_print, prefer_const_constructors, sort_child_properties_last
+
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
-      
+      home: Scaffold(
+        body: Column(
+          children: [
+            SizedBox(
+              height: 55,
+              width: 300,
+              child: ElevatedButton(
+                onPressed: () {
+                  print("Button Pressed");
+                },
+                child: Text("Find Your Job"),
+                
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(2, 118, 234, 100),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    )
+                    
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
