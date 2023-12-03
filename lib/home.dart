233c9fb02f5f47cb6ee52967fd1c0abee8c1d541
mirsadkt <mirsadkt@gmail.com>
2023-12-03 17:08:11 +0530

    First Page ready

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_print, prefer_const_constructors, sort_child_properties_last
+// ignore_for_file: avoid_print, prefer_const_constructors, avoid_unnecessary_containers, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
@@ -15,30 +15,74 @@ class _MyHomePageState extends State<MyHomePage> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: [
-
-            
-            SizedBox(
-              height: 55,
-              width: 300,
-              child: ElevatedButton(
-                onPressed: () {
-                  print("Button Pressed");
-                },
-                child: Text("Find Your Job"),
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Color.fromRGBO(2, 118, 234, 100),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    )
-                    
+          backgroundColor: Colors.white,
+          body: Column(
+            children: [
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 45),
+                  child: Image(
+                    image: AssetImage("assets/imgtwo.png"),
+                    height: 270,
+                    width: 270,
+                  ),
                 ),
               ),
-            )
-          ],
-        ),
-      ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Text("Welcome",
+                    style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w700,
+                        color: Color.fromARGB(255, 0, 0, 0))),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(13.0),
+                child: Text(
+                  "Get Hired with ease using our app",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                      color: Color.fromARGB(255, 0, 0, 0)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 70),
+                child: Text(
+                  "Take the first step towards your\ndream with our app",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 108, 105, 108),
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 75),
+                child: Container(
+                  height: 60,
+                  width: 272,
+                  child: ElevatedButton(
+                      onPressed: () {
+                        print("Go to login ");
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: Color.fromARGB(255, 10, 117, 205),
+                      ),
+                      child: Text(
+                        "Find your Job",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                        ),
+                      )),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
