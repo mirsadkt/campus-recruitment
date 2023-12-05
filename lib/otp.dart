86eabd3a9ff06d852d07a09b97b110c8345a370e
mirsadkt <mirsadkt@gmail.com>
2023-12-05 22:20:37 +0530

    otp page completed

---
+++ lib/otp.dart
@@ -0,0 +1,190 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, sort_child_properties_last, sized_box_for_whitespace, avoid_print
+
+import 'package:flutter/material.dart';
+
+class OnetimePassword extends StatefulWidget {
+  const OnetimePassword({super.key});
+
+  @override
+  State<OnetimePassword> createState() => _OnetimePasswordState();
+}
+
+class _OnetimePasswordState extends State<OnetimePassword> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "OTP screen",
+      home: Scaffold(
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(14.0),
+                    child: Container(
+                      height: 39,
+                      width: 39,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          print("Back button Pressed");
+                        },
+                        child: Icon(
+                          Icons.arrow_back_ios,
+                          size: 15,
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color.fromARGB(255, 150, 200, 241),
+                          foregroundColor: Colors.blue.shade600,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20)),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Row(
+                children: [
+                  Text(
+                    "OTP Verification",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 26,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: Text(
+                "Enter the verification code we sent to your \n \t \t  \t \t \t  \t \t \t Mobile Number",
+                style: TextStyle(
+                    color: Colors.grey.shade400, fontWeight: FontWeight.w500),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 85),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide(
+                                  width: 4,
+                                )),
+                            contentPadding: EdgeInsets.all(10)),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              width: 4,
+                            ),
+                          ),
+                          contentPadding: EdgeInsets.all(10),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(
+                              width: 4,
+                            ),
+                          ),
+                          contentPadding: EdgeInsets.all(10),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      child: TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide(
+                                width: 4,
+                              ),),
+                          contentPadding: EdgeInsets.all(10),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 210),
+              child: Container(
+                height: 43,
+                width: 200,
+                child: TextButton(onPressed: (){},
+                 child: Text("Proceed",
+                 style: TextStyle(
+                  fontSize: 18,
+                 ),
+                 ),
+                 style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: Colors.blue.shade800,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                 ),            
+                 ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 17),
+              child: Text("Didn't recieve code?",
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+              ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(onPressed: (){},
+               child: Text("Resend OTP",
+               style: TextStyle(
+                color: Colors.blue.shade800,
+                decoration: TextDecoration.underline, 
+               ),
+               
+               ),
+               ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/signup.dart
+++ lib/signup.dart
@@ -246,7 +246,7 @@ class _SignupPageState extends State<SignupPage> {
                       ],
                     ),
                   ),
-                )
+                ),
               ],
             ),
           ),
