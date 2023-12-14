fe2402ffb521ec4154b15ff923505629adff1482
mirsadkt <mirsadkt@gmail.com>
2023-12-14 14:05:49 +0530

    notification page completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@
 import 'package:campus_recruitment/pages/home.dart';
 import 'package:campus_recruitment/pages/landing.dart';
 import 'package:campus_recruitment/pages/nonotifications.dart';
+import 'package:campus_recruitment/pages/notification.dart';
 import 'package:campus_recruitment/pages/otp.dart';
 import 'package:campus_recruitment/pages/pastevents.dart';
 import 'package:campus_recruitment/pages/signup.dart';
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const NoNotify(),
+      home: const Notify(),
     );
   }
 }
